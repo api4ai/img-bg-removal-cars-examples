71b039826661f4a0850035d855b7e71812a800db
Jahangir Shah <shah@api4.ai>
2022-04-19 14:29:06 +0300

    Remove "noqa" comments.

--- python/aiohttp/sample.py
+++ python/aiohttp/sample.py
@@ -27,14 +27,13 @@ RAPIDAPI_KEY = ''
 # * fg-mask - Mask image.
 RESULT_MODE = 'fg-image-shadow'
 
-
 OPTIONS = {
     'demo': {
-        'url': f'https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode={RESULT_MODE}',  # noqa
+        'url': f'https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode={RESULT_MODE}',
         'headers': {'A4A-CLIENT-APP-ID': 'sample'}
     },
     'rapidapi': {
-        'url': f'https://cars-image-background-removal.p.rapidapi.com/v1/results?mode={RESULT_MODE}',  # noqa
+        'url': f'https://cars-image-background-removal.p.rapidapi.com/v1/results?mode={RESULT_MODE}',
         'headers': {'X-RapidAPI-Key': RAPIDAPI_KEY}
     }
 }
@@ -42,7 +41,7 @@ OPTIONS = {
 
 async def main():
     """Entry point."""
-    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-cars-1.jpg'  # noqa
+    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-cars-1.jpg'
 
     # response = None
     async with aiohttp.ClientSession() as session:
@@ -54,11 +53,11 @@ async def main():
             data = {'image': open(image, 'rb')}
         # Make request.
         async with session.post(OPTIONS[MODE]['url'],
-                                data=data,  # noqa
+                                data=data,
                                 headers=OPTIONS[MODE]['headers']) as response:
             resp_json = await response.json()
 
-        img_b64 = resp_json['results'][0]['entities'][0]['image'].encode('utf8')  # noqa
+        img_b64 = resp_json['results'][0]['entities'][0]['image'].encode('utf8')
 
         path_to_image = os.path.join('result.png')
         with open(path_to_image, 'wb') as img:
