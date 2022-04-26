d265fe9a15d42590adc1d80d477c0f4369da0171
Dmitri Lapin <lapin@api4.ai>
2022-04-26 12:35:52 +0300

    Added NodeJS/Axios sample

---
+++ nodejs/axios/README.md
@@ -0,0 +1,52 @@
+# API4AI car image background removal sample
+
+This directory contains a minimalistic sample that sends requests to the API4AI Car Image Background Removal API.
+The sample is implemented in `nodejs` using [axios](https://www.npmjs.com/package/axios) and [from-data](https://www.npmjs.com/package/form-data) packages.
+
+
+## Overview
+
+This API provides segmentation photos of cars with consequent background removal.
+
+
+## Getting started
+
+*NodeJS version `8.3.0` or greater is required to run this script.*
+
+Preinstall dependencies before use:
+
+```bash
+npm i --production
+```
+
+Try sample with default args:
+
+```bash
+node index.js
+```
+
+Try sample with your local image:
+
+```bash
+node index.js image.jpg
+```
+
+
+## About API keys
+
+This demo by default sends requests to free endpoint at `demo.api4ai.cloud`.
+Demo endpoint is rate limited and should not be used in real projects.
+
+Use [RapidAPI marketplace](https://rapidapi.com/api4ai-api4ai-default/api/cars-image-background-removal/details) to get the API key. The marketplace offers both
+free and paid subscriptions.
+
+[Contact us](https://api4.ai/contacts?utm_source=img_bg_removal_cars_example_repo&utm_medium=readme&utm_campaign=examples) in case of any questions or to request a custom pricing plan
+that better meets your business requirements.
+
+
+## Links
+
+* 📩 Email: hello@api4.ai
+* 🔗 Website: [http://api4.ai](https://api4.ai?utm_source=img_bg_removal_cars_example_repo&utm_medium=readme&utm_campaign=examples)
+* 🤖 Telegram demo bot: https://t.me/a4a_cars_img_bg_removal_bot
+* 🔵 Our API at RapidAPI marketplace: https://rapidapi.com/api4ai-api4ai-default/api/cars-image-background-removal/details
---
+++ nodejs/axios/index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+// Example of using API4AI car image background removal.
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios').default
+const FormData = require('form-data')
+
+// Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+// For more details visit:
+//   https://api4.ai
+
+// Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+// For more details visit:
+//   https://rapidapi.com/api4ai-api4ai-default/api/cars-image-background-removal/details
+const MODE = 'demo'
+
+// Your RapidAPI key. Fill this variable with the proper value if you want
+// to try api4ai via RapidAPI marketplace.
+const RAPIDAPI_KEY = ''
+
+// Processing mode influences returned result. Supported values are:
+// * fg-image-shadow - Foreground image with shadow added.
+// * fg-image - Foreground image.
+// * fg-mask - Mask image.
+const RESULT_MODE = 'fg-image-shadow'
+
+const OPTIONS = {
+  demo: {
+    url: `https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode=${RESULT_MODE}`,
+    headers: { 'A4A-CLIENT-APP-ID': 'sample' }
+  },
+  rapidapi: {
+    url: `https://cars-image-background-removal.p.rapidapi.com/v1/results?mode=${RESULT_MODE}`,
+    headers: { 'X-RapidAPI-Key': RAPIDAPI_KEY }
+  }
+}
+
+// Parse args: path or URL to image.
+const image = process.argv[2] || 'https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-cars-1.jpg'
+
+// Preapare request: form.
+const form = new FormData()
+if (image.includes('://')) {
+  // Data from image URL.
+  form.append('url', image)
+} else {
+  // Data from local image file.
+  const fileName = path.basename(image)
+  form.append('image', fs.readFileSync(image), fileName)
+}
+
+// Preapare request: headers.
+const headers = {
+  ...OPTIONS[MODE].headers,
+  ...form.getHeaders()
+}
+
+// Make request.
+axios.post(OPTIONS[MODE].url, form, { headers })
+  .then(function (response) {
+    const imgBase64 = Buffer
+      .from(response.data.results[0].entities[0].image, 'base64')
+
+    fs.writeFile('result.png', imgBase64, () => {
+      console.log('💬 The "result.png" image is saved to the current directory.')
+    })
+  })
