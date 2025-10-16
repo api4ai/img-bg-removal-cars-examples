099b856c3f990456d05ddf5a904c22faae284daa
Vladislav Gubarev <gubarev@api4.ai>
2025-10-16 18:04:46 +0300

    Updated python samples: Added "normal" mode

--- python/aiohttp/sample.py
+++ python/aiohttp/sample.py
@@ -10,15 +10,17 @@ import sys
 import aiohttp
 
 
-# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
-# For more details visit:
-#   https://api4.ai
+# Use 'demo' mode just to try api4ai for free. ⚠️ Free demo is rate limited and must not be used in real projects.
+#
+# Use 'normal' mode if you have an API Key from the API4AI Developer Portal. This is the method that users should normally prefer.
 #
 # Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
 # For more details visit:
 #   https://rapidapi.com/api4ai-api4ai-default/api/cars-image-background-removal/details
 MODE = 'demo'
 
+# Your API4AI key. Fill this variable with the proper value if you have one.
+API4AI_KEY = ''
 
 # Your RapidAPI key. Fill this variable with the proper value if you want
 # to try api4ai via RapidAPI marketplace.
@@ -32,8 +34,11 @@ RESULT_MODE = 'fg-image-shadow'
 
 OPTIONS = {
     'demo': {
-        'url': f'https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode={RESULT_MODE}',
-        'headers': {'A4A-CLIENT-APP-ID': 'sample'}
+        'url': f'https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode={RESULT_MODE}'
+    },
+    'normal': {
+        'url': f'https://api4ai.cloud/img-bg-removal/v1/cars/results?mode={RESULT_MODE}',
+        'headers': {'X-API-KEY': API4AI_KEY}
     },
     'rapidapi': {
         'url': f'https://cars-image-background-removal.p.rapidapi.com/v1/results?mode={RESULT_MODE}',
@@ -44,7 +49,7 @@ OPTIONS = {
 
 async def main():
     """Entry point."""
-    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-cars-1.jpg'
+    image = sys.argv[1] if len(sys.argv) > 1 else 'https://static.api4.ai/samples/img-bg-removal-cars-1.jpg'
 
     async with aiohttp.ClientSession() as session:
         if '://' in image:
@@ -56,7 +61,7 @@ async def main():
         # Make request.
         async with session.post(OPTIONS[MODE]['url'],
                                 data=data,
-                                headers=OPTIONS[MODE]['headers']) as response:
+                                headers=OPTIONS[MODE].get('headers')) as response:
             resp_json = await response.json()
 
         img_b64 = resp_json['results'][0]['entities'][0]['image'].encode('utf8')
