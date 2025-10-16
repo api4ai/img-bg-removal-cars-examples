d7547731366c98f4e470867eb060abd882c78aae
Vladislav Gubarev <gubarev@api4.ai>
2025-10-16 17:39:57 +0300

    Updated bash/curl sample: Added "normal" mode

--- bash/curl/sample.sh
+++ bash/curl/sample.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 
-# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
-# For more details visit:
-#   https://api4.ai
+# Use 'demo' mode just to try api4ai for free. ⚠️ Free demo is rate limited and must not be used in real projects.
+#
+# Use 'normal' mode if you have an API Key from the API4AI Developer Portal. This is the method that users should normally prefer.
 #
 # Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
 # For more details visit:
 #   https://rapidapi.com/api4ai-api4ai-default/api/cars-image-background-removal/details
 MODE="demo"
 
+# Your API4AI key. Fill this variable with the proper value if you have one.
+API4AI_KEY=""
 
 # Your RapidAPI key. Fill this variable with the proper value if you want
 # to try api4ai via RapidAPI marketplace.
@@ -26,7 +28,9 @@ RESULT_MODE="fg-image-shadow"
 # Define URL and headers.
 if [[ "${MODE}" == "demo" ]]; then
     URL="https://demo.api4ai.cloud/img-bg-removal/v1/cars/results?mode=${RESULT_MODE}"
-    HEADERS="A4A-CLIENT-APP-ID: sample"  # optional header
+elif [[ "${MODE}" == "normal" ]]; then
+    URL="https://api4ai.cloud/img-bg-removal/v1/cars/results?mode=${RESULT_MODE}"
+    HEADERS="X-API-KEY: ${API4AI_KEY}"
 elif [[ "${MODE}" == "rapidapi" ]]; then
     URL="https://cars-image-background-removal.p.rapidapi.com/v1/results?mode=${RESULT_MODE}"
     HEADERS="X-RapidAPI-Key: ${RAPIDAPI_KEY}"
@@ -37,7 +41,7 @@ fi
 
 
 # Path or URL to image.
-IMAGE=${1:-"https://storage.googleapis.com/api4ai-static/samples/img-bg-removal-cars-1.jpg"}
+IMAGE=${1:-"https://static.api4.ai/samples/img-bg-removal-cars-1.jpg"}
 
 # POST.
 if [[ "${IMAGE}" =~ "://" ]]; then
